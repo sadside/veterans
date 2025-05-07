a97724ded8e4b788b9d08f94cb2a4f876bca857c
sadside <sadside1319@gmail.com>
2025-05-07 15:38:31 +0700

    fix: remove Vercel adapter from astro.config.mjs and adjust output settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,10 @@ import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 
-import vercel from '@astrojs/vercel'
-
 // https://astro.build/config
 export default defineConfig({
     integrations: [react(), tailwind({ applyBaseStyles: false })],
     output: 'server',
-    adapter: vercel(),
     image: {
         domains: ['81.31.247.179'],
         remotePatterns: [
